1ddb2c8652cb06cf1d4d5f5b6a1f82957d5a15c6
arturious <arturbakhtygereyev@gmail.com>
2026-07-28 06:15:01 +0500

    Bump version to 1.0.3

--- Sources/browser/UpdateChecker.swift
+++ Sources/browser/UpdateChecker.swift
@@ -10,7 +10,7 @@ import AppKit
 final class UpdateChecker {
     static let shared = UpdateChecker()
 
-    static let currentVersion = "1.0.2"
+    static let currentVersion = "1.0.3"
     private static let repo = "arturious/browser"
 
     private init() {}
